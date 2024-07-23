108b0df1741713bb2b8c4c9749ea5b0893392dc9
Oleg Nanovsky <oociso@ya.ru>
2024-07-24 06:32:03 +0700

    add_APLclass

---
+++ src/APL_classes.py
@@ -0,0 +1,48 @@
+import requests, json
+from data.config import HH_VACANCIES_URL, HH_HEADERS, COUNT_HH, PATH_LOG, currency_change, PAGES
+
+class HH_API():
+    '''Класс для поиска вакансий на сайте HH.ru по id_работодателя '''
+    def __init__(self, employer_id: str) -> None:
+        self.employer_id = employer_id
+        self.__url = HH_VACANCIES_URL
+        self.__headers = HH_HEADERS
+        self.__page = PAGES
+
+    def __str__(self) -> str:
+        return ("Класс для поиска вакансий на сайте HH.ru по id работодателя")
+
+    def get_response(self, page):
+        '''Принимает параметры и выдает список найденных вакансий в формате Json'''
+        self.page = page
+        self.response = requests.get(url=self.__url, headers=self.__headers, params={"employer_id": self.employer_id, "per_page": COUNT_HH, 'page': self.page, "archived": False})
+        return self.response.json()
+
+    def preparation_vacancy_one_company(self):
+        '''Получает данные в формате Json и возвращает общий список словарей с только необходимыми параметрами вакансий'''
+        all_vacancy = []
+        for page in range(self.__page):
+            vacancy_hh = self.get_response(page)
+            for vacancy in vacancy_hh["items"]:
+                try:
+                    all_vacancy.append(dict(vacancy_id=int(vacancy['id']),
+                        employer_id=self.employer_id, vacancy_name=vacancy.get('name'),
+                        salary_from=(self.currency_exchange(vacancy['salary']['from'],
+                                   vacancy['salary']['currency'])) if vacancy.get('salary')
+                                            else None, url=vacancy['alternate_url']))
+                except TypeError:
+                    with open(PATH_LOG, "a", encoding="UTF-8") as file:
+                        file.write(json.dumps(vacancy, ensure_ascii=False))
+                    continue
+        return all_vacancy
+
+    def currency_exchange(self, salary, currenty):
+        '''Получает данные о сумме и валюте, и возвращает сумму конвертируемую в рубли при необходимости'''
+        if salary != None and salary != 0:
+            if currenty == "RUR" or "rub":
+                data_change = salary
+            else:
+                data_change = salary * currency_change[currenty.upper()]
+        else:
+            data_change = None
+        return data_change
---
+++ src/creating_database.py
@@ -0,0 +1,51 @@
+import psycopg2
+from data.config import PARAMS_BD
+
+class CREATE_DB():
+    '''Класс для создания и наполнения базы данных и таблиц в ней'''
+    def __init__(self) -> None:
+        self.params = PARAMS_BD
+        self.database_name = 'vacansies_hh'
+
+    def __str__(self) -> str:
+        return ("Класс для создания и наполнения базы данных и таблиц в ней")
+
+    def create_database(self):
+        """Создание базы данных и таблиц для сохранения данных о каналах и видео."""
+
+        conn = psycopg2.connect(dbname='postgres', **self.params)
+
+        with conn.cursor() as cur:
+            conn.autocommit = True
+            cur.execute(f"DROP DATABASE IF EXISTS {self.database_name}")
+            cur.execute(f"CREATE DATABASE {self.database_name}")
+            conn.commit()
+        conn.close()
+
+        conn = psycopg2.connect(dbname=self.database_name, **self.params)
+
+        with conn.cursor() as cur:
+            cur.execute('''
+            CREATE TABLE companies(
+                    company_id INT PRIMARY KEY, company_title VARCHAR(35) NOT NULL);
+            CREATE TABLE vacancies (
+                    vacancy_id INT PRIMARY KEY,
+                    company_id INT REFERENCES companies(company_id),
+                    vacancy_title VARCHAR(155) NOT NULL,
+                    salary_from INT,
+                    vacancy_url TEXT);
+                    ''')
+            conn.commit()
+        conn.close()
+
+    def filling_table(self, data_for_table, table_name):
+        '''Наполняет таблицу данными из json файла'''
+        conn = psycopg2.connect(dbname=self.database_name, **self.params)
+        with conn.cursor() as cur:
+            for line in data_for_table:
+                values = ("%s," * len(line))[:-1]
+                try:
+                    cur.execute(f"INSERT INTO {table_name} VALUES ({values})", tuple(line.values()))
+                except psycopg2.errors.UniqueViolation:
+                    pass
+                conn.commit()
